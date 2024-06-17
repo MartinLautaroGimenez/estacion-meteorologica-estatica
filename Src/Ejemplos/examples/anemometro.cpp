449eb3c7c116ab562347aa016ee3758c9db8be46
Mario-dango <mp.robots@gmail.com>
2024-06-17 06:32:26 -0300

    Se avanzó con los programas de ejemplo de Anemómetro y Veleta, falta verificarlos en simulador

--- Src/Ejemplos/examples/anemometro.cpp
+++ Src/Ejemplos/examples/anemometro.cpp
@@ -3,16 +3,27 @@
 #define pinAnemometer 5
 
 void interruption();
+float velocidad();
+long t0,t1 = millis();
 
 void setup(){
-    pinMode(pinAnemometer, INPUT_PULLDOWN);
-    attachInterrupt(pinAnemometer, interruption, RISING);
+    pinMode(pinAnemometer, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pinAnemometer), interruption, RISING);
 }
 
 void loop(){
-    
+    printf("hola, ando verificando el anemómetro!");
+    printf("la velocidad es %f.2", velocidad());
+    delay(1000);
 }
 
+float velocidad(){
+    int brazo = 7;
+    return ((brazo*2*PI)/(t1-t0));  // se devuelve el resultado de velocidad tangencial
+}
+
+// Lectura de tiempos para deltaT
 void interruption(){
-    
+    t0 = t1;        //  Refresco el valor del tiempo anterior
+    t1 = millis();  //  Cargo el nuevo tiempo transcurrido
 }
--- Src/Ejemplos/examples/veleta.cpp
+++ Src/Ejemplos/examples/veleta.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
 
-void setup(){
+int pines[8] = {
+    9       // pinNorte
+    , 8     // pinNorEste
+    , 7     // pinNorOeste
+    , 6     // pinOeste
+    , 5     // pinEste
+    , 4     // pinSur
+    , 3     // pinSurOeste
+    , 2     // pinSurEste
+};
 
+boolean direction[8];
+char* ultimaPos = "Narnia";
+
+void setup(){    
+    Serial.begin(9600);
+    for (size_t i = 0; i < sizeof(direction); i++){
+        pinMode(pines[i], INPUT);
+    }
 }
 
 void loop(){
-    
+    for (size_t i = 0; i < sizeof(direction); i++){
+        direction[i] = digitalRead(pines[i]);
+        if (direction[i] == 1){
+            // switch-case para cargar las últimas lecturas
+            switch (i){
+            case 0:
+                ultimaPos = "Norte";
+                break;        
+            case 1:
+                ultimaPos = "NorEste";
+                break;        
+            case 2:
+                ultimaPos = "NorOeste";
+                break;        
+            case 3:
+                ultimaPos = "Oeste";
+                break;        
+            case 4:
+                ultimaPos = "Este";
+                break;        
+            case 5:
+                ultimaPos = "Sur";
+                break;        
+            case 6:
+                ultimaPos = "SurOeste";
+                break;            
+            case 7:
+                ultimaPos = "SurEste";
+                break;        
+            default: // en default notificar que se debe de ver la última posición
+                Serial.println("revisar el último valor registrado!");
+                break;
+            }
+        }
+    }
+    delay(200);    
 }
