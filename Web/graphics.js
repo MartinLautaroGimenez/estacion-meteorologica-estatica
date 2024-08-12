8e014d2588b568e3342dcd775b97c1e2be1dda26
Mario Papetti <61957863+Mario-dango@users.noreply.github.com>
2024-08-12 00:58:44 -0300

    Update graphics.js

--- Web/graphics.js
+++ Web/graphics.js
@@ -15,9 +15,12 @@ async function fetchData() {
 
         const data = await response.json();
         const ultimos10Datos = data.slice(-10);
+        
+        const numMuestras = document.getElementById('numMuestras').value;
+        const ultimosNdatos = data.slice(-numMuestras); // Filtrar por muestras seleccionadas
         console.log(data); // Registro para verificar que los datos se obtienen correctamente
         displayData(data); // Función para mostrar los datos (puedes definirla más adelante)
-        createTemperatureHumidityChart(ultimos10Datos); // Función para crear gráfico de temperatura y humedad
+        createTemperatureHumidityChart(ultimosNdatos); // Función para crear gráfico de temperatura y humedad
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
