88f497f20997d52f689a047000a3acda8f8668d2
Martín Gimenez <mal.gimenez@alumno.etec.um.edu.ar>
2024-11-02 10:13:59 -0300

    Cambios en el dropdown: Se le añadio Test a la pestaña Datos, y se configuró una cookie

--- Web/app.js
+++ Web/app.js
@@ -19,91 +19,108 @@ themeToggler.onclick = function() {
 // Comprobar si hay un tema guardado en el almacenamiento local
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
-    // Aplicar tema oscuro al cargar la página
     document.body.classList.add('dark-theme');
-    themeToggler.querySelector('span:nth-child(1)').classList.remove('active'); // Desactivar primer span
-    themeToggler.querySelector('span:nth-child(2)').classList.add('active'); // Activar segundo span
+    themeToggler.querySelector('span:nth-child(1)').classList.remove('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.add('active');
 }
 
-window.addEventListener('load', function() {
-    document.getElementById('categoriaDropdown').value = "emetec"; // Valor default "emetec"
-  });
+// Función para establecer cookies
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
 
-// Esperar a que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://emetec.wetec.um.edu.ar/datos';
-    const apiTempUrl = 'https://emetec.wetec.um.edu.ar/datostruchos';
-
-    async function obtenerDatos(url) {
-        try {
-            const response = await fetch(url, {
-                headers: {
-                    'Access-Control-Allow-Origin': '*'
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error('Error en la solicitud HTTP: ' + response.statusText);
-            }
-
-            const data = await response.json();
-            const ultimos10Datos = data.slice(-10);
-
-            const ultimoDato = ultimos10Datos[0];
-            document.getElementById('datoNumero').textContent = ultimoDato["Dato N°"];
-            document.getElementById('fecha').textContent = ultimoDato["Fecha"];
-            document.getElementById('battery').textContent = ultimoDato["Nivel de bateria"] + ' V';
-            document.getElementById('temperatura').textContent = ultimoDato["Temperatura Promedio"] + ' °C';
-            document.getElementById('stermica').textContent = ultimoDato["Sensacion T"] + ' °C';
-            document.getElementById('presion').textContent = ultimoDato["Presion"] + ' hPa';
-            document.getElementById('humedad').textContent = ultimoDato["Humedad"] + ' %';
-            document.getElementById('nivelLuz').textContent = ultimoDato["Nivel de luz"] + ' lm';
-            document.getElementById('procio').textContent = ultimoDato["Temperatura punto de rocio"] + ' °C';
-            document.getElementById('ppmco2').textContent = ultimoDato["ppm CO2"] + ' ppm';
-            document.getElementById('calidadAire').textContent = ultimoDato["Calidad del aire"];
-            document.getElementById('velocidadViento').textContent = ultimoDato["Velocidad del viento"];
-            document.getElementById('direccionViento').textContent = ultimoDato["Direccion del viento"];
-            document.getElementById('sensorLluvia').textContent = ultimoDato["Sensor de lluvia"];
-
-            document.getElementById('fecha2').textContent = ultimoDato["Fecha"];
-            document.getElementById('fecha3').textContent = ultimoDato["Fecha"];
-            document.getElementById('fecha4').textContent = ultimoDato["Fecha"];
-            document.getElementById('fecha5').textContent = ultimoDato["Fecha"];
-
-            const tablaDetallesCuerpo = document.getElementById('tablaDetallesCuerpo');
-            tablaDetallesCuerpo.innerHTML = '';
-
-            ultimos10Datos.forEach(dato => {
-                const fila = document.createElement('tr');
-                fila.innerHTML = `
-                    <td>${dato["Dato N°"]}</td>
-                    <td>${dato["Fecha"]}</td>
-                    <td>${dato["Nivel de bateria"]} V</td>
-                    <td>${dato["Temperatura Promedio"]} °C</td>
-                    <td>${dato["Presion"]} hPa</td>
-                    <td>${dato["Humedad"]} %</td>
-                    <td>${dato["Nivel de luz"]} lm</td>
-                    <td>${dato["Calidad del aire"]}</td>
-                    <td>${dato["ppm CO2"]} ppm</td>
-                `;
-                tablaDetallesCuerpo.appendChild(fila);
-            });
-
-        } catch (error) {
-            console.error('Error al obtener los datos:', error);
+// Función para obtener el valor de una cookie
+function getCookie(name) {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+    name = name + "=";
+    for (let cookie of cookies) {
+        cookie = cookie.trim();
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
         }
     }
+    return "";
+}
 
-    // Event listener para el dropdown
-    categoriaDropdown.addEventListener('change', function() {
-        const selectedValue = categoriaDropdown.value;
-        if (selectedValue === 'emetec') {
-            obtenerDatos(apiUrl); // Usar la URL de temperatura
-        } else if (selectedValue === 'test') {
-            obtenerDatos(apiTempUrl); // Usar la URL por defecto
-        }
-    });
-
-    // Llamar a obtenerDatos al cargar la página para mostrar datos iniciales
-    obtenerDatos(apiUrl);
+// Cargar el valor del dropdown desde la cookie si existe
+window.addEventListener('load', function() {
+    const savedValue = getCookie('categoriaDropdown');
+    if (savedValue) {
+        categoriaDropdown.value = savedValue;
+    } else {
+        categoriaDropdown.value = "emetec"; // Valor default si no hay cookie
+    }
+    categoriaDropdown.dispatchEvent(new Event('change')); // Disparar cambio para cargar datos iniciales
 });
+
+// Event listener para el dropdown
+categoriaDropdown.addEventListener('change', function() {
+    const selectedValue = categoriaDropdown.value;
+    setCookie('categoriaDropdown', selectedValue, 30); // Guardar selección en cookie
+    if (selectedValue === 'emetec') {
+        obtenerDatos(apiUrl);
+    } else if (selectedValue === 'test') {
+        obtenerDatos(apiTempUrl);
+    }
+});
+
+// Función para obtener datos
+async function obtenerDatos(url) {
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Error en la solicitud HTTP: ' + response.statusText);
+        }
+        const data = await response.json();
+        const ultimos10Datos = data.slice(-10);
+
+        const ultimoDato = ultimos10Datos[0];
+        document.getElementById('datoNumero').textContent = ultimoDato["Dato N°"];
+        document.getElementById('fecha').textContent = ultimoDato["Fecha"];
+        document.getElementById('battery').textContent = ultimoDato["Nivel de bateria"] + ' V';
+        document.getElementById('temperatura').textContent = ultimoDato["Temperatura Promedio"] + ' °C';
+        document.getElementById('stermica').textContent = ultimoDato["Sensacion T"] + ' °C';
+        document.getElementById('presion').textContent = ultimoDato["Presion"] + ' hPa';
+        document.getElementById('humedad').textContent = ultimoDato["Humedad"] + ' %';
+        document.getElementById('nivelLuz').textContent = ultimoDato["Nivel de luz"] + ' lm';
+        document.getElementById('procio').textContent = ultimoDato["Temperatura punto de rocio"] + ' °C';
+        document.getElementById('ppmco2').textContent = ultimoDato["ppm CO2"] + ' ppm';
+        document.getElementById('calidadAire').textContent = ultimoDato["Calidad del aire"];
+        document.getElementById('velocidadViento').textContent = ultimoDato["Velocidad del viento"];
+        document.getElementById('direccionViento').textContent = ultimoDato["Direccion del viento"];
+        document.getElementById('sensorLluvia').textContent = ultimoDato["Sensor de lluvia"];
+        document.getElementById('fecha2').textContent = ultimoDato["Fecha"];
+        document.getElementById('fecha3').textContent = ultimoDato["Fecha"];
+        document.getElementById('fecha4').textContent = ultimoDato["Fecha"];
+        document.getElementById('fecha5').textContent = ultimoDato["Fecha"];
+
+        const tablaDetallesCuerpo = document.getElementById('tablaDetallesCuerpo');
+        tablaDetallesCuerpo.innerHTML = '';
+
+        ultimos10Datos.forEach(dato => {
+            const fila = document.createElement('tr');
+            fila.innerHTML = `
+                <td>${dato["Dato N°"]}</td>
+                <td>${dato["Fecha"]}</td>
+                <td>${dato["Nivel de bateria"]} V</td>
+                <td>${dato["Temperatura Promedio"]} °C</td>
+                <td>${dato["Presion"]} hPa</td>
+                <td>${dato["Humedad"]} %</td>
+                <td>${dato["Nivel de luz"]} lm</td>
+                <td>${dato["Calidad del aire"]}</td>
+                <td>${dato["ppm CO2"]} ppm</td>
+            `;
+            tablaDetallesCuerpo.appendChild(fila);
+        });
+    } catch (error) {
+        console.error('Error al obtener los datos:', error);
+    }
+}
